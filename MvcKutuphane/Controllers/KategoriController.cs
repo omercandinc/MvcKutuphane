03fbca9c1d4d23663e7c806b1f64d761db396e8b
Omercan <omercandinc4152@hotmail.com>
2020-08-04 16:06:39 +0300

    Yeni düzenlemeler

--- MvcKutuphane/Controllers/KategoriController.cs
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class KategoriController : Controller
     {
-        // GET: Kategori
+		// GET: Kategori
+		DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
-            return View();
+			var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
         }
-    }
+		[HttpPost]
+		public ActionResult KategoriEkle(TBLKATEGORI p)
+		{
+			db.TBLKATEGORI.Add(p);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public ActionResult KategoriEkle()
+		{
+			return View();
+		}
+	}
 }
